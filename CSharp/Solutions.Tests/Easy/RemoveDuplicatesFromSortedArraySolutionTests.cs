cbf87e86bac5d3bfabdfd8ad44edd3cf7821007d
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2022-12-02 23:22:15 +0100

    Easy - Remove Duplicates from Sorted Array

---
+++ CSharp/Solutions.Tests/Easy/RemoveDuplicatesFromSortedArraySolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.Easy.Tests
+{
+	[TestClass()]
+	public class RemoveDuplicatesFromSortedArraySolutionTests
+	{
+		[DataTestMethod]
+		[DataRow(new int[] { 1, 1, 2}, new int[] { 1, 2 })]
+		[DataRow(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, new int[] { 0, 1, 2, 3, 4 })]
+		public void RemoveDuplicatesTest(int[] input, int[] expected)
+		{
+			int k = RemoveDuplicatesFromSortedArraySolution.RemoveDuplicates(input);
+
+			Assert.AreEqual(expected.Length, k);
+
+			for (var i = 0; i < k; i++)
+				Assert.AreEqual(expected[i], input[i]);
+		}
+	}
+}
---
+++ CSharp/Solutions/Easy/RemoveDuplicatesFromSortedArraySolution.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.Easy
+{
+	public static class RemoveDuplicatesFromSortedArraySolution
+	{
+		/// <summary>
+		/// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <returns></returns>
+		public static int RemoveDuplicates(int[] nums)
+		{
+			var index = 1;
+
+			for (var i = 1; i < nums.Length; i++)
+			{
+				if (nums[index - 1] != nums[i])
+				{
+					nums[index] = nums[i];
+					index++;
+				}
+			}
+
+			return index;
+		}
+	}
+}
