227938cbb96dba9919ab8593dcfdef17e82b6652
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2022-12-08 23:57:02 +0100

    Easy - Remove Element

---
+++ CSharp/Solutions.Tests/Easy/RemoveElementSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.Easy.Tests
+{
+	[TestClass]
+	public class RemoveElementSolutionTests
+	{
+		[DataTestMethod]
+		[DataRow(new int[] { 3, 2, 2, 3 }, 3, new int[] { 2, 2 })]
+		[DataRow(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, new int[] { 0, 0, 1, 3, 4 })]
+		[DataRow(new int[] { }, 0, new int[] { })]
+		[DataRow(new int[] { 2 }, 3, new int[] { 2 })]
+		public void RemoveElementTest(int[] input, int valueToRemove, int[] expected)
+		{
+			int k = RemoveElementSolution.RemoveElement(input, valueToRemove);
+
+			Assert.AreEqual(expected.Length, k);
+			Array.Sort(input, 0, k);
+
+			for (var i = 0; i < k; i++)
+				Assert.AreEqual(expected[i], input[i]);
+		}
+	}
+}
---
+++ CSharp/Solutions/Easy/RemoveElementSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace Solutions.Easy
+{
+	public class RemoveElementSolution
+	{
+		/// <summary>
+		/// https://leetcode.com/problems/remove-element/
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <param name="val"></param>
+		/// <returns></returns>
+		public static int RemoveElement(int[] nums, int val)
+		{
+			if (!nums.Any())
+				return 0;
+
+			var end = nums.Length - 1;
+
+			for (var i = 0; i < end; i++)
+				if (nums[i] == val)
+				{
+					if (nums[end] == val)
+					{
+						end--;
+						i--;
+					}
+					else
+						(nums[i], nums[end]) = (nums[end], nums[i]);
+				}
+
+			return nums[end] == val ? end : end + 1;
+		}
+	}
+}
